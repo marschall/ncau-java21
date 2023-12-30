48ac01c2ead0b2d7e3e12ff534dfde58892021bc
Philippe Marschall <philippe.marschall@gmail.com>
2023-12-30 14:40:05 +0100

    sequenced tests

--- ncau-java21-api/src/test/java/com/netcetera/ncau/java21/api/SequencedCollectionsTests.java
+++ ncau-java21-api/src/test/java/com/netcetera/ncau/java21/api/SequencedCollectionsTests.java
@@ -1,7 +1,9 @@
 package com.netcetera.ncau.java21.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,24 @@ class SequencedCollectionsTests {
     for (String letter : letters.reversed()) {
       assertNotNull(letter);
     }
+  }
+
+  @Test
+  void firstLast() {
+    List<String> letters = new ArrayList<>(List.of("A", "B", "C"));
+    assertEquals("A", letters.getFirst());
+    assertEquals("C", letters.getLast());
+
+    letters.addFirst("z");
+    letters.addLast("D");
+    assertEquals(List.of("z", "A", "B", "C", "D"), letters);
+  }
+
+  @Test
+  void reversedAddFirst() {
+    List<String> letters = new ArrayList<>(List.of("A", "B", "C"));
+    letters.reversed().add("z");
+    assertEquals(List.of("z", "A", "B", "C"), letters);
   }
 
 }
