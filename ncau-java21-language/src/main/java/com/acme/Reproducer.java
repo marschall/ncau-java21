ca9f15b9d730caced8c1d044bd157e638a216fa9
Philippe Marschall <philippe.marschall@gmail.com>
2023-12-31 09:27:32 +0100

    reduce

--- ncau-java21-language/src/main/java/com/acme/Reproducer.java
+++ ncau-java21-language/src/main/java/com/acme/Reproducer.java
@@ -3,15 +3,13 @@ package com.acme;
 import java.util.Objects;
 
 import com.acme.Reproducer.Expression.ConstantExpression;
-import com.acme.Reproducer.Expression.PlusExpression;
-import com.acme.Reproducer.Expression.TimesExpression;
 
 public class Reproducer {
 
   public static void main(String[] args) {
     SExpressionPrinter printer = new SExpressionPrinter21Record();
-    Expression twoPlusThree = new PlusExpression(new ConstantExpression(2), new ConstantExpression(3));
-    System.out.println(printer.print(twoPlusThree));
+    Expression constant = new ConstantExpression(2);
+    System.out.println(printer.print(constant));
   }
 
   interface SExpressionPrinter {
@@ -33,21 +31,6 @@ public class Reproducer {
     private void printTo(Expression e, StringBuilder buffer) {
       if (e instanceof ConstantExpression(int i)) {
         buffer.append(i);
-      } else {
-        buffer.append('(');
-        if (e instanceof PlusExpression(Expression a, Expression b)) {
-          buffer.append("+ ");
-          printTo(a, buffer);
-          buffer.append(' ');
-          printTo(b, buffer);
-        }
-        if (e instanceof TimesExpression(Expression a, Expression b)) {
-          buffer.append("* ");
-          printTo(a, buffer);
-          buffer.append(' ');
-          printTo(b, buffer);
-        }
-        buffer.append(')');
       }
     }
   }
@@ -61,24 +44,6 @@ public class Reproducer {
       @Override
       public int evaluate() {
         return i;
-      }
-
-    }
-
-    record PlusExpression(Expression a, Expression b) implements Expression {
-
-      @Override
-      public int evaluate() {
-        return Math.addExact(a.evaluate(), b.evaluate());
-      }
-
-    }
-
-    record TimesExpression(Expression a, Expression b) implements Expression {
-
-      @Override
-      public int evaluate() {
-        return Math.multiplyExact(a.evaluate(), b.evaluate());
       }
 
     }
